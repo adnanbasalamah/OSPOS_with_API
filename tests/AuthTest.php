015424d4aa9a7843ce9d6508c7934868b4acfd04
gasing-tambah <adnan.b@gmail.com>
2026-05-24 16:52:16 +0700

    test(api): Add comprehensive JWT unit tests (9 tests, 21 assertions)

--- tests/AuthTest.php
+++ tests/AuthTest.php
@@ -57,4 +57,80 @@ class AuthTest extends TestCase
         $this->expectException(\Firebase\JWT\SignatureInvalidException::class);
         JWT::decode($token, new Key(self::JWT_SECRET, 'HS256'));
     }
+
+    public function testTokenBlacklistHashIsDeterministic(): void
+    {
+        $token = 'test-token-123';
+        $hash1 = hash('sha256', $token);
+        $hash2 = hash('sha256', $token);
+        $this->assertEquals($hash1, $hash2);
+        $this->assertEquals(64, strlen($hash1));
+    }
+
+    public function testTokenBlacklistDifferentTokensHashDifferently(): void
+    {
+        $hash1 = hash('sha256', 'token-a');
+        $hash2 = hash('sha256', 'token-b');
+        $this->assertNotEquals($hash1, $hash2);
+    }
+
+    public function testJwtTokenContainsRequiredClaims(): void
+    {
+        $payload = [
+            'iss' => 'kasirbaru',
+            'iat' => time(),
+            'exp' => time() + 3600,
+            'sub' => 42,
+            'username' => 'testuser',
+        ];
+
+        $token = JWT::encode($payload, self::JWT_SECRET, 'HS256');
+        $decoded = JWT::decode($token, new Key(self::JWT_SECRET, 'HS256'));
+
+        $this->assertObjectHasProperty('iss', $decoded);
+        $this->assertObjectHasProperty('iat', $decoded);
+        $this->assertObjectHasProperty('exp', $decoded);
+        $this->assertObjectHasProperty('sub', $decoded);
+        $this->assertEquals('kasirbaru', $decoded->iss);
+        $this->assertIsInt($decoded->iat);
+        $this->assertIsInt($decoded->exp);
+        $this->assertIsInt($decoded->sub);
+    }
+
+    public function testJwtTokenTamperedPayloadDetected(): void
+    {
+        $payload = [
+            'iss' => 'kasirbaru',
+            'iat' => time(),
+            'exp' => time() + 3600,
+            'sub' => 1,
+        ];
+
+        $token = JWT::encode($payload, self::JWT_SECRET, 'HS256');
+        $parts = explode('.', $token);
+        $tamperedPayload = rtrim(base64url_encode(json_encode(['sub' => 999])), '=');
+        $fakeToken = $parts[0] . '.' . $tamperedPayload . '.' . $parts[2];
+
+        $this->expectException(\Firebase\JWT\SignatureInvalidException::class);
+        JWT::decode($fakeToken, new Key(self::JWT_SECRET, 'HS256'));
+    }
+
+    public function testJwtRejectsMalformedToken(): void
+    {
+        $this->expectException(\DomainException::class);
+        JWT::decode('not.a.token', new Key(self::JWT_SECRET, 'HS256'));
+    }
+
+    public function testJwtRejectsEmptyToken(): void
+    {
+        $this->expectException(\UnexpectedValueException::class);
+        JWT::decode('', new Key(self::JWT_SECRET, 'HS256'));
+    }
+}
+
+if (!function_exists('base64url_encode')) {
+    function base64url_encode(string $data): string
+    {
+        return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
+    }
 }
