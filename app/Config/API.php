4302e16fe4ee9e3427fc2a3e6a71b5f9e56a95d5
gasing-tambah <adnan.b@gmail.com>
2026-05-24 16:31:48 +0700

    feat(config): Add API configuration for JWT and CORS

---
+++ app/Config/API.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Config;
+
+use CodeIgniter\Config\BaseConfig;
+
+class API extends BaseConfig
+{
+    public string $jwt_secret = 'kasirbaru_jwt_secret_change_me';
+
+    public int $jwt_expiry = 3600;
+
+    public string $jwt_algorithm = 'HS256';
+
+    public array $cors = [
+        'allowed_origins' => ['*'],
+        'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        'allowed_headers' => ['Content-Type', 'Authorization', 'X-Requested-With'],
+        'max_age' => 7200,
+    ];
+}
